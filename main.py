8f1676b7a38fe4e8020cda1108d3d04978331019
Alsy <alexanderace43@gmail.com>
2024-01-10 21:14:14 +0000

    End of day 1 of tutoring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_leap_year(year):
+def is_leap_year_ad_num(year):
     if (year/4) == float(year//4):
        return "Leap year!"
     elif (year/400) == float(year//400):
@@ -6,12 +6,21 @@ def is_leap_year(year):
     else:
        return "Not a leap year :("
 
+def is_leap_year_bc_num(year):
+    if (year/4)-1 == float(year//4)-1:
+
+
 if __name__ == "__main__":
-   year = int(input("Give me a year and I will determine whether it is a leap year or not: "))
+   year = input("Give me a year and I will determine whether it is a leap year or not: ")
+
+
+    if year.isdigit() or "AD" in year.upper():
+        print(is_leap_year_ad_num(int(year)))
+    else:
+        print(is_leap_year_bc_num(year))
 
    print("You typed " + str(year))
 
-   print(is_leap_year(year))
 
 
 
--- unittests.py
+++ unittests.py
@@ -3,16 +3,33 @@ import main
 
 class Testing(unittest.TestCase):
 
-    def test_leap_year(self):
-        year_result = main.is_leap_year(2012)
+    def test_leap_year_ad(self):
+        year_result = main.is_leap_year_ad_num(2012)
         self.assertEqual(year_result, "Leap year!")
 
-    def test_special_leap_year(self):
-        year_result = main.is_leap_year(2000)
+    def test_special_leap_year_ad(self):
+        year_result = main.is_leap_year_ad_num(2000)
         self.assertEqual(year_result, "Leap year!")
 
-    def test_not_leap_year(self):
-        year_result = main.is_leap_year(2011)
+    def test_not_leap_year_ad(self):
+        year_result = main.is_leap_year_ad_num(2011)
+        self.assertEqual(year_result, "Not a leap year :(")
+
+
+    def test_leap_year_bc(self):
+        year_result = main.is_leap_year_bc_num(-399)
+        self.assertEqual(year_result, "Leap year!")
+
+    def test_not_leap_year_bc(self):
+        year_result = main.is_leap_year_bc_num(-400)
+        self.assertEqual(year_result, "Not a leap year :(")
+
+    def test_leap_year_bc(self):
+        year_result = main.is_leap_year_bc_num(-8)
+        self.assertEqual(year_result, "Leap year!")
+
+   def test_not_leap_year_bc(self):
+        year_result = main.is_leap_year_bc_num(-6)
         self.assertEqual(year_result, "Not a leap year :(")
 
 
