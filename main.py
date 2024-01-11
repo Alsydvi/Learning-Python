ac070027463cc2a060ef9f5bc9c5408237a80f3d
Alsydvi <alexanderace43@gmail.com>
2024-01-11 18:36:38 +0000

    Added functionality for BC years. Now also accepts if you add 'AD' at the end.

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 def is_leap_year_ad_num(year):
-    if (year/4) == float(year//4):
+    year = int(year.upper().replace("AD", ""))
+    if ((int(year))/4) == float(year//4):
        return "Leap year!"
-    elif (year/400) == float(year//400):
+    elif ((int(year))/400) == float(year//400):
        return "Leap year!"
     else:
        return "Not a leap year :("
 
 def is_leap_year_bc_num(year):
-    if (year/4)-1 == float(year//4)-1:
+    year = int(year.upper().replace("BC", ""))
+    if ((year-1)/4) == float((year-1)//4):
+        return "Leap year!"
+    elif ((year-1)/400) == float((year-1)//400):
+        return "Leap year!"
+    else:
+        return "Not a leap year :("
 
 
 if __name__ == "__main__":
-   year = input("Give me a year and I will determine whether it is a leap year or not: ")
+   year = input("Give me a year with numbers and I will determine whether it is a leap year or not, you may add 'BC' for bc years. : ")
 
 
-    if year.isdigit() or "AD" in year.upper():
-        print(is_leap_year_ad_num(int(year)))
-    else:
-        print(is_leap_year_bc_num(year))
+if year.isdigit() or "AD" in year.upper():
+        print(is_leap_year_ad_num((year)))
 
-   print("You typed " + str(year))
+if "BC" in year.upper():
+        print(is_leap_year_bc_num((year)))
+
+print("You typed " + str(year))
 
 
 
